e3e4bd395ac2192e874532821dceaf9b23992234
Collin Marando <collin.marando@hotmail.com>
2024-09-29 15:48:11 -0400

    ch16, e16.5

--- ch16.v
+++ ch16.v
@@ -314,3 +314,326 @@ Proof.
   all: try assumption.
   all: exists q; lia.
 Qed.
+
+Close Scope Z_scope.
+
+(* End 16.2 *)
+
+Module assoc.
+
+Inductive bin: Set := node : bin -> bin -> bin | leaf: nat -> bin.
+
+Fixpoint flatten_aux (t fin:bin) {struct t} : bin :=
+  match t with
+  | node t1 t2 => flatten_aux t1 (flatten_aux t2 fin)
+  | x => node x fin
+  end.
+
+Fixpoint flatten (t:bin) : bin := 
+  match t with
+  | node t1 t2 => flatten_aux t1 (flatten t2)
+  | x => x
+  end.
+
+Fixpoint bin_nat (t:bin) : nat :=
+  match t with
+  | node t1 t2 => bin_nat t1 + bin_nat t2
+  | leaf n => n
+  end.
+
+(* Exercise 16.3 *)
+(* Prove the theorems flatten aux_valid, flatten_valid, and flatten_valid_2. *)
+
+Theorem flatten_aux_valid: forall t t':bin,
+  bin_nat t + bin_nat t' = bin_nat (flatten_aux t t').
+Proof.
+  induction t; intros t'; simpl; auto.
+  rewrite <- Nat.add_assoc.
+  rewrite IHt2.
+  rewrite IHt1.
+  reflexivity.
+Qed.
+
+Theorem flatten_valid: forall t:bin,
+  bin_nat t = bin_nat (flatten t).
+Proof.
+  induction t; simpl; auto.
+  rewrite <- flatten_aux_valid.
+  auto.
+Qed.
+
+Theorem flatten_valid_2: forall t t':bin,
+  bin_nat (flatten t) = bin_nat (flatten t') -> bin_nat t = bin_nat t'.
+Proof.
+  intros; rewrite (flatten_valid t); rewrite (flatten_valid t'); auto.
+Qed.
+
+(* End 16.3 *)
+
+Section assoc_eq.
+
+Import List.
+
+Variables (A : Set) (f : A->A->A)
+  (assoc: forall x y z:A, f x (f y z) = f (f x y) z).
+
+Fixpoint bin_A (l:list A) (def:A) (t:bin) {struct t} : A :=
+  match t with
+  | node t1 t2 => f (bin_A l def t1) (bin_A l def t2)
+  | leaf n => nth n l def
+  end.
+
+(* Exercise 16.4 *)
+(* Using the hypothesis f_assoc, prove the three theorems flatten_aux_valid_A,
+  flatten_valid_A, and flatten_valid_A_2. *)
+
+Theorem flatten_aux_valid_A: forall (l:list A) (def:A) (t t':bin),
+  f (bin_A l def t) (bin_A l def t') = bin_A l def (flatten_aux t t').
+Proof.
+  induction t; simpl; intros; auto.
+  rewrite <- assoc.
+  rewrite IHt2.
+  rewrite IHt1.
+  auto.
+Qed.
+
+Theorem flatten_valid_A: forall (l:list A) (def:A) (t:bin),
+  bin_A l def t = bin_A l def (flatten t).
+Proof.
+  induction t; simpl; auto.
+  rewrite <- flatten_aux_valid_A, IHt2.
+  auto.
+Qed.
+
+Theorem flatten_valid_A_2: forall (t t':bin) (l:list A) (def:A),
+  bin_A l def (flatten t) = bin_A l def (flatten t') -> bin_A l def t = bin_A l def t'.
+Proof.
+  intros.
+  rewrite (flatten_valid_A _ _ t).
+  rewrite (flatten_valid_A _ _ t').
+  auto.
+Qed.
+
+End assoc_eq.
+
+(* End 16.4 *)
+
+Section tactic.
+
+  Ltac term_list f l v := 
+    match v with
+    | f ?X1 ?X2 => 
+      let l1 := term_list f l X2 in
+        term_list f l1 X1
+    | ?X1 => constr:(cons X1 l) 
+    end.
+
+  Ltac compute_rank l n v := match l with
+    | cons ?X1 ?X2 =>
+      let tl := constr:(X2) in
+        match constr:(X1 = v) with
+        | ?X1 = ?X1 => n
+        | _ => compute_rank tl (S n) v
+        end
+    end.
+  
+  Ltac model_aux l f v := match v with
+    | f ?X1 ?X2 => 
+      let r1 := model_aux l f X1 with r2 := model_aux l f X2 in
+        constr:(node r1 r2)
+    | ?X1 => let n := compute_rank l 0 X1 in
+        constr:(leaf n)
+    end.
+  
+  Ltac model_A A f def v := 
+    let l := term_list f (nil (A:=A)) v in
+    let t := model_aux l f v in
+    constr:(bin_A A f l def t).
+  
+  (* At the end of the tactic, we also remove the zero. *)
+  Ltac assoc_eq A f assoc_thm :=
+    match goal with
+    | |- @eq A ?X1 ?X2 =>
+      let term1 := model_A A f X1 X1 with
+        term2 := model_A A f X1 X2 in
+        (change (term1 = term2));
+        apply flatten_valid_A_2 with ( 1 := assoc_thm ); 
+        auto
+    end.
+  
+  Theorem reflection_test3:
+  forall (x y z t u : Z),  (x * (((y * z) )* (t * u)) = (x * ( y)) * (z * (t * u)))%Z.
+  Proof.
+    intros; assoc_eq Z Zmult Zmult_assoc.
+  Qed.
+
+End tactic.
+
+End assoc.
+
+(* Exercise 16.5 *)
+(* Adapt the tactic to the case where the binary operation has a neutral element,
+  like zero for addition. It should be able to prove equalities of the form
+  "(x+0)+(y+(z+0)) = x+(y+(z+0))" *)
+
+Module neutral_element.
+
+Inductive bin: Set :=
+  | node: bin -> bin -> bin 
+  | leaf: nat -> bin
+  | neutral: bin.
+
+Fixpoint flatten_aux (t fin:bin) {struct t} : bin :=
+  match t with
+  | node t1 t2 => flatten_aux t1 (flatten_aux t2 fin)
+  | x => node x fin
+  end.
+
+Fixpoint flatten (t:bin) : bin := 
+  match t with
+  | node t1 t2 => flatten_aux t1 (flatten t2)
+  | x => x
+  end.
+
+Fixpoint remove_neutral1 (t : bin) : bin :=
+ match t with
+   leaf n => leaf n
+  | neutral => neutral
+  | node neutral t' => remove_neutral1 t'
+  | node t t' => node t (remove_neutral1 t')
+ end.
+ 
+Fixpoint remove_neutral2 (t : bin) : bin :=
+ match t with
+   leaf n => leaf n
+  | neutral => neutral
+  | node t neutral => t
+  | node t1 t2 => node t1 (remove_neutral2 t2)
+ end.
+
+Definition remove_neutral (t : bin) := remove_neutral2 (remove_neutral1 t).
+
+Section assoc_eq.
+
+Import List.
+
+Variables (A : Set) (f : A->A->A) (zero_A : A)
+  (assoc: forall x y z:A, f x (f y z) = f (f x y) z)
+  (zero_left: forall (x : A), f zero_A x = x)
+  (zero_right: forall (x : A), f x zero_A = x).
+
+Fixpoint bin_A (l:list A) (t:bin) {struct t} : A :=
+  match t with
+  | node t1 t2 => f (bin_A l t1) (bin_A l t2)
+  | leaf n => nth n l zero_A
+  | neutral => zero_A
+  end.
+
+Theorem flatten_aux_valid_A: forall (l:list A) (t t':bin),
+  f (bin_A l t) (bin_A l t') = bin_A l (flatten_aux t t').
+Proof.
+  induction t; simpl; intros; auto.
+  rewrite <- assoc.
+  rewrite IHt2.
+  rewrite IHt1.
+  auto.
+Qed.
+
+Theorem flatten_valid_A: forall (l:list A) (t:bin),
+  bin_A l t = bin_A l (flatten t).
+Proof.
+  induction t; simpl; auto.
+  rewrite <- flatten_aux_valid_A, IHt2.
+  auto.
+Qed.
+
+Theorem flatten_valid_A_2: forall (t t':bin) (l:list A),
+  bin_A l (flatten t) = bin_A l (flatten t') -> bin_A l t = bin_A l t'.
+Proof.
+  intros.
+  rewrite (flatten_valid_A _ t).
+  rewrite (flatten_valid_A _ t').
+  auto.
+Qed.
+
+Theorem remove_neutral1_valid_A:
+ forall (l : list A) (t : bin), bin_A l (remove_neutral1 t) = bin_A l t.
+Proof.
+intros l t; elim t; auto.
+intros t1; case t1; simpl.
+- intros t1' t1'' IHt1 t2 IHt2; rewrite IHt2; trivial.
+- intros n IHt1 t2 IHt2; rewrite IHt2; trivial.
+- intros IHt1 t2 IHt2; rewrite zero_left; trivial.
+Qed.
+ 
+Theorem remove_neutral2_valid_A:
+ forall (l : list A) (t : bin),  bin_A l (remove_neutral2 t) = bin_A l t.
+Proof.
+  intros l t; elim t; auto.
+  intros t1 IHt1 t2; case t2; simpl.
+  - intros t2' t2'' IHt2; rewrite IHt2; trivial.
+  - auto.
+  - intros IHt2; rewrite zero_right; trivial.
+Qed. 
+ 
+Theorem remove_neutral_equal: forall (t t' : bin) (l : list A),
+  bin_A l (remove_neutral t) = bin_A l (remove_neutral t') -> bin_A l t = bin_A l t'.
+Proof.
+  unfold remove_neutral; intros t t' l.
+  repeat rewrite remove_neutral2_valid_A.
+  repeat rewrite remove_neutral1_valid_A.
+  trivial.
+Qed.
+
+End assoc_eq.
+
+Ltac term_list f zero l v := 
+    match v with
+    | f ?X1 ?X2 => 
+      let l1 := term_list f zero l X2 in
+        term_list f zero l1 X1
+    | zero => l
+    | ?X1 => constr:(cons X1 l) 
+    end.
+
+Ltac compute_rank l n v := match l with
+  | cons ?X1 ?X2 =>
+    let tl := X2 in
+      match constr:(X1 = v) with
+      | ?X1 = ?X1 => n
+      | _ => compute_rank tl (S n) v
+      end
+  end.
+
+Ltac model_aux l f zero v := match v with
+  | f ?X1 ?X2 => 
+    let r1 := model_aux l f zero X1 with r2 := model_aux l f zero X2 in
+      constr:(node r1 r2)
+  | zero => neutral
+  | ?X1 => let n := compute_rank l 0 X1 in
+      constr:(leaf n)
+  end.
+
+Ltac model_A A f zero v := 
+  let l := term_list f zero (nil (A:=A)) v in
+  let t := model_aux l f zero v in
+  constr:(bin_A A f zero l t).
+
+Ltac assoc_eq A f zero assoc_thm neutral_left_thm neutral_right_thm :=
+  match goal with
+  | |- @eq A ?X1 ?X2 =>
+    let term1 := model_A A f zero X1 with
+      term2 := model_A A f zero X2 in
+      (change (term1 = term2); 
+      apply flatten_valid_A_2 with ( 1 := assoc_thm );
+      apply remove_neutral_equal
+        with ( 1 := neutral_left_thm ) ( 2 := neutral_right_thm ); auto)
+  end.
+
+Theorem reflection_test_neutral: forall (x y z t u : Z),
+  ((x + 0) + (y + (z + 0)) = x + (y + (z + 0)))%Z.
+Proof.
+  intros; assoc_eq Z Z.add 0%Z Z.add_assoc Z.add_0_l Z.add_0_r.
+Qed.
+
+(* End 16.5 *)
